89034731701831cd900a5d42b5bc3f21e9e389f5
king-dong-gun <a01021290607@gmail.com>
2024-04-03 17:58:11 +0900

    sql 테이블 정리

--- src/sql/day02.sql
+++ src/sql/day02.sql
@@ -1,178 +1,201 @@
-CREATE TABLE EMP (
-                     EMPNO       NUMBER(4) NOT NULL,     -- 사원번호
-                     ENAME       VARCHAR2(10),           -- 사원이름 (VARCHAR2(10)으로 수정)
-                     JOB         VARCHAR2(9),            -- 업무
-                     MGR         NUMBER(4),              -- 사수번호
-                     HIREDATE    DATE,                   -- 입사일
-                     SAL         NUMBER(7,2),            -- 월급
-                     COMM        NUMBER(7,2),            -- 커미션
-                     DEPTNO      NUMBER(2)               -- 부서번호
-);
+-- SELECT 문 연습
 
+-- 전체 컬럼 조회
+SELECT *
+FROM emp;
 
-SELECT * FROM emp;
+-- 특정 컬럼 조회(사원번호, 사원명, 월급)
+SELECT EMPNO, ENAME, SAL
+FROM emp;
 
+-- 컬럼에 별칭(alias)을 사용 : AS
+-- 별칭에 공백이 있으면 "" 로 묶어줘야 한다.
+SELECT ENAME AS 사원명, SAL AS 월급
+FROM emp;
 
+-- AS 생략 가능
+SELECT ENAME 사원명, SAL 월급
+FROM emp;
 
-INSERT INTO emp VALUES (7499, 'ALLEN', 'SALEMAN', 7698, to_date('20_02_1981', 'dd-mm-yyyy'), 1600, 300, 30);
+-- 사원번호, 사원이름, 월급, 연봉을 구하고 컬럼명은 "사원번호" , "사원이름", "월급", "연봉" 으로 출력
+SELECT EMPNO as 사원번호, ENAME as 사원이름, SAL as 월급, (SAL * 12) as 연봉
+From EMP;
 
-INSERT INTO emp VALUES (7521, 'WARD', 'SALEMAN', 7698, to_date('22_02_1981', 'dd-mm-yyyy'), 1250, 500, 30);
+-- 특정컬럼 중복을 제거 : DISTINCT
+SELECT DISTINCT JOB
+FROM EMP;
 
-INSERT INTO emp VALUES (7566, 'JONES', 'MANAGER', 7839, to_date('02_04_1981', 'dd-mm-yyyy'), 2975, NULL, 20);
+SELECT ALL JOB, DEPTNO
+FROM EMP;
 
-INSERT INTO emp VALUES (7654, 'MARTIN', 'SALEMAN', 7698, to_date('29_09_1981', 'dd-mm-yyyy'), 1250, 1400, 30);
+-- 가상의 테이블 : DUAL
+SELECT 'RONALDO'
+FROM dual;
 
-INSERT INTO emp VALUES (7698, 'BLAKE', 'MANAGER', 7698, to_date('01_05_1981', 'dd-mm-yyyy'), 2850, NULL, 30);
+-- 컬럼명에 연산이 들어가면 결과값이 행에 출력
+SELECT 2 + 3
+FROM DUAL;
+SELECT 'ABC', 2 + 3
+FROM DUAL;
 
-INSERT INTO emp VALUES (7782, 'CLARK', 'MANAGER', 7839, to_date('09_06_1981','dd-mm-yyyy'), 2450, NULL, 10);
+-- 연산결과 별칭을 주면 결과값을 행에 출력하고 컬럼명은 별칭
+SELECT 2 + 3 AS RESULT
+FROM DUAL;
+SELECT 5 * 5 AS VIVID
+FROM DUAL;
 
-INSERT INTO emp VALUES (7788, 'SCOTT', 'ANALYST', 7566, to_date('13_07_1987', 'dd-mm-yyyy'), 3000, NULL, 20);
+-- 숫자와 문자가 연산이 되면 숫자로 타입이 정해진다.
+SELECT 1 + '5'
+FROM DUAL;
+SELECT '1' + '5'
+FROM DUAL;
 
-INSERT INTO emp VALUES (7839, 'KING', 'PRESIDENT', NULL, to_date('17_11_1981', 'dd-mm-yyyy'), 5000, NULL, 10);
+-- 문자의 연결은 || 를 사용
+SELECT 1 || '5'
+FROM DUAL;
 
-INSERT INTO emp VALUES (7844, 'TURNER', 'SALESMAN', 7698, to_date('08_09_1981', 'dd-mm-yyyy'), 1500, NULL, 30);
+-- 사원명과 업무로 연결(SMITH, CLERK) 표시 컬럼명은 EMPLOYEE AND JOB
+SELECT '(' || ENAME || ', ' || JOB || ')' AS "EMPLOYEE AND JOB"
+FROM EMP;
 
-INSERT INTO emp VALUES (7876, 'ADAMS', 'CLERK', 7788, to_date('13_07_1987', 'dd-mm-yyyy'), 1100, NULL, 20);
+-- 사원별 연간 총수입을 나타내시오(별칭은 연간총수입)
+SELECT ENAME, SAL * 12 + COMM AS 연간총수입
+FROM emp;
+-- NULL 값 때문에 데이터가 제대로 안나옴
+-- NULL 값은 0으로 계산하고 더한다.
+SELECT ENAME,
+       CASE
+           WHEN COMM IS NULL THEN (SAL + 0)
+           ELSE (SAL + COMM) end as 연간총수입
+FROM emp;
 
-INSERT INTO emp VALUES (7900, 'JAMES', 'CLERK', 7698, to_date('13_12_1981', 'dd-mm-yyyy'), 950, NULL, 30);
+-- 정렬 : ODER BY 컬럼명
+-- 디폴트 값은 오름 차순(ASC)§
+SELECT *
+FROM EMP
+ORDER BY SAL;
+-- 내림차순은 DESC
+SELECT *
+FROM EMP
+ORDER BY SAL DESC;
 
-INSERT INTO emp VALUES (7902, 'FORM', 'ANALYST', 7566, to_date('03_12_1981', 'dd-mm-yyyy'), 3000, NULL, 20);
+-- WHERE 절 : 데이터 조회시 조건을 걸어야 할 때 쓰인다.
+SELECT *
+FROM EMP
+WHERE EMPNO = 7839;
 
-INSERT INTO emp VALUES (7934, 'MILLER', 'CLERK', 7782, to_date('23_01_1982', 'dd-mm-yyyy'), 1300, NULL, 10);
+-- 사번이 7698인 사원명과 업무, 급여를 출력
+SELECT ENAME, JOB, SAL
+FROM EMP
+WHERE EMPNO = 7698;
 
-COMMIT;
-SELECT * FROM emp;
+-- SMITH 의 사원명, 부서, 월급
+SELECT ENAME, JOB, SAL
+FROM EMP
+WHERE ENAME = 'SMITH';
 
-CREATE TABLE DEPT (
-                      DEPTNO  NUMBER(2),      -- 부서번호
-                      DNAME   VARCHAR2(4),    -- 부서명
-                      LOC     VARCHAR2(13)    -- 지역
-);
+-- 월급이 3000 이 아닌 사람 : !=
+SELECT *
+FROM EMP
+WHERE SAL != 3000;
 
-SELECT * FROM dept;
+-- 월급이 5000이 아닌 사람 : <>(ANSI 표준)
+SELECT *
+FROM EMP
+WHERE SAL <> 5000;
 
-INSERT INTO dept VALUES (10, 'ACCOUNTING', 'NEW YORK');
+-- 월급이 6000이 아닌 사람 : NOT
+SELECT *
+FROM EMP
+WHERE NOT SAL = 6000;
 
-ALTER TABLE dept MODIFY dname varchar2(14);
+-- 문자비교도 가능, 첫 글자를 기준
+SELECT *
+FROM EMP
+WHERE ENAME >= 'M';
 
-INSERT INTO dept VALUES (20, 'RESEARCH', 'DALLAS');
-INSERT INTO dept VALUES (30, 'SELES', 'CHICAGO');
-INSERT INTO dept VALUES (40, 'OPERATIONS', 'BOSTON');
+-- 월급이 2500 이상 3000 미만의 사원,입사일,월급 출력
+SELECT ENAME, HIREDATE, SAL
+FROM EMP
+where SAL >= 2500
+  AND SAL < 3000;
 
-CREATE TABLE bonus (
-                       ENAME   VARCHAR2(10),
-                       JOB     VARCHAR2(9),
-                       SAL     NUMBER,
-                       COMM    NUMBER
-);
+-- 월급이 2000 이상 3000 이하가 아닌 사원,입사일,월급 출력
+SELECT ENAME, SAL, HIREDATE
+FROM EMP
+WHERE NOT SAL >= 2000
+  AND SAL <= 3000;
 
-CREATE TABLE salegrade (    --급여정도
-                           GRADE   NUMBER,
-                           LOSAL   NUMBER,
-                           HISAL   NUMBER
-);
+-- BETWEEN 연산자 : 컬럼명 BETWEEN 100 AND 200 -> 100과 200사이의 데이터(이상,이하)
+SELECT *
+FROM EMP
+WHERE SAL BETWEEN 2500 AND 3000;
 
-INSERT INTO salegrade s VALUES (1, 700, 1200);
-INSERT INTO salegrade s VALUES (2, 1201, 1400);
-INSERT INTO salegrade s VALUES (3, 1401, 2000);
-INSERT INTO salegrade s VALUES (4, 2001, 3000);
-INSERT INTO salegrade s VALUES (5, 3001, 9999);
+-- 81년 5월 1일과 81년 12월 3일 사이에 입사사원을 출력
+SELECT ENAME, SAL, HIREDATE
+FROM EMP
+WHERE HIREDATE BETWEEN '1981-05-01' AND '1981-12-3';
+-- TO_DATE() 를 사용하여 날짜 비교
+SELECT ENAME, SAL, HIREDATE
+FROM EMP
+WHERE HIREDATE BETWEEN TO_DATE('19810501', 'YYYYMMDD') and TO_DATE('19811203', 'YYYYMMDD');
 
-SELECT * FROM emp;  -- 전체조회
+-- 1987년도 입사한 사원명, 월급, 입사날짜를 출력
+SELECT ENAME, SAL, HIREDATE
+FROM EMP
+WHERE HIREDATE BETWEEN TO_DATE('19810101', 'YYYYMMDD') and TO_DATE('19811231', 'YYYYMMDD');
 
-SELECT empno, ename, sal FROM emp;  --필요 행만 가져오기 (사원번호, 사원이름, 월급)
+-- 여러 조건을 충족 시켜야 할 때 : 컬럼명 IN(조건1, 조건2, ....)
+SELECT ENAME, JOB, SAL
+FROM EMP
+WHERE JOB IN ('MANAGER', 'CLERK', 'SALESMAN');
 
-SELECT ename "이름", sal "월급" FROM emp;   -- ename -> 이름, sal -> 월급 변경
+-- 반대는 NOT IN()
+-- 사원번호가 7566, 7782, 7934 인 사원을 제외한 사원번호, 사원명, 월급을 출력
+SELECT EMPNO, ENAME, SAL
+FROM EMP
+WHERE EMPNO NOT IN (7566, 7782, 7934);
 
+-- 부서번호가 30이고, 월급이 2000 이하를 받는 81년 5월 1일 이전에 입사한 사원의 이름,급여,부서번호,입사일 출력
+SELECT ENAME, SAL, DEPTNO, HIREDATE
+FROM EMP
+WHERE DEPTNO = 30
+  AND SAL <= 2000
+  AND HIREDATE < TO_DATE('19810501', 'YYYYMMDD');
 
--- 사원번호, 사원이름, 월급, 연봉을 구하고 컬럼명은 사원번호, 사원이름, 월급, 연봉으로 출력
+-- 부서번호가 10 또는 30인 사원이 월급이 2000 ~ 5000 사이의 사원명, 급여, 부서번호
+SELECT ENAME, SAL, DEPTNO
+FROM EMP
+WHERE DEPTNO IN (10, 30)
+  AND SAL BETWEEN 2000 AND 5000;
 
-SELECT empno "사원번호", ename"이름",sal"월급", sal*12 "연봉" FROM emp;
+-- JOB 이 MANAGER 또는 SALESMAN 이고 급여가 1600, 2975, 2850이 아닌 사원명, 입사일, 급여, 부서번호 출력
+SELECT ENAME, JOB, HIREDATE, SAL, DEPTNO
+FROM EMP
+WHERE JOB IN ('MANAGER', 'SALESMAN')
+  AND SAL NOT IN (1600, 2975, 2850);
 
-SELECT DISTINCT job FROM emp e;     -- 중복데이터 제거
+/* ================================================================================= */
+-- 특정 키워드를 찾을 때는 LIKE 를 사용
+-- S% : S 로 시작되는 모든 문자
+SELECT *
+FROM EMP
+WHERE ENAME LIKE 'S%';
 
-SELECT DISTINCT job, deptno FROM emp e;
+-- _는 한글자를 의미한다. _L% 은 두번쨰 글자에 L이 오는 모든 문자
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '_L%';
 
-SELECT ALL job, deptno FROM emp e;
+-- 1) 사원이름 중에 'S' 가 포합되지 않는 부서번호가 20인 사원의 이름, 부서번호, 조회
+SELECT ENAME, DEPTNO
+FROM EMP
+WHERE ENAME NOT LIKE '%S%'
+  AND DEPTNO = 20;
 
-SELECT 'ABC' FROM dual;
-SELECT 2 + 3 FROM dual;
-SELECT 'ABC', 2 + 3 FROM dual;
-SELECT 2+3 AS RESULT FROM dual;
-SELECT 1+'5' FROM dual;
-SELECT 1||'5' FROM dual;
-
--- 사원명과 업무로 연결(smith, clerk) 표시, 컬럼명은 employee and job 출력
-
-SELECT '( '|| ename || ', ' || job || ')' as "employee and job" from emp;
-
--- 사원별 연간 총수입을 조회, 별칭은 연간 총 수입으로 출력
-SELECT  ename, sal*12 + comm  AS "연간총수입" FROM emp;
-
-SELECT * FROM emp e ORDER BY sal;   -- 오름차순
-
-SELECT * FROM emp e ORDER BY empno;
-
-SELECT * FROM emp ORDER BY sal DESC;    -- 내림차순
-
-/*===================================================================*/
--- 조건을 추가하는 where 절
-select * from emp e where EMPNO = 7839;
-
--- 사원번호 7698인 사원명과 업무, 급여 출력
-select ename, job, sal  from emp e where EMPNO = 7698;
--- smith의 사원명, 부서, 월급
-select ENAME, JOB, SAL from emp e where ENAME = 'SMITH';
-select * from emp e where SAL = 3000;
-select * from emp e where SAL != 3000;
-select * from emp e where SAL ^= 4000;
--- <> 5000이 아닌 레코드 출력
-select * from emp e where SAL <> 5000;
-select * from emp e where not SAL = 6000;
--- 첫문자가 M과 같거나 큰 문자열 출력
-select * from emp e where ENAME >= 'M';
-
--- 월급이 2500이상 3000미만의 사원명과 입사일, 월급 출력
-select ENAME, HIREDATE, SAL from emp e where 2500 <= SAL and SAL < 3000;
-
--- 월급이 2000이상 3000이하에 포함되어 있지 않는 사원 출력
-select * from EMP e where sal between 2500 and 3000;
-
--- 81년 5월 1일과 동년 12월 3일 사이에 입사한 사원, 급여, 입사일 출력
-select ENAME, SAL, HIREDATE from EMP e where HIREDATE between '1981-05-01' and '1981-12-03';
-select ENAME, sal, HIREDATE from EMP e where HIREDATE between to_date('19810501', 'yyyymmdd') and to_date('19811203', 'yyyymmdd');
-
--- 1987년도에 입사한 사원명, 월급, 입사일 출력
-select ENAME, SAL, HIREDATE from EMP e where HIREDATE between to_date('19870101', 'yyyymmdd') and to_date('19871212','yyyymmdd');
-
--- 직업이 manager, clerk, salesman인 사람 출력
-select * from EMP e where job = 'MANAGER' or job = 'CLERK' or job = 'SALESMAN' ;
-
-select * from EMP e where job in ('MANAGER', 'CLERK', 'SALESMAN');
-
--- 사번이 7566, 7782, 7934인 사원을 제외한 사번, 사원명, 급여 출력
-select EMPNO, ENAME, SAL from EMP e where EMPNO not in (7566, 7782, 7934);
-
--- 부서번호 30에서 근무하고 급여 2000 이하를 받는 81년 5월 1일 이전에 입사한 사원의 사원명, 급여, 부서번호, 입사일 출력
-select ENAME, SAL, DEPTNO, HIREDATE from emp e where DEPTNO = 30 and SAL <= 2000 and HIREDATE > '1981-05-01';
-
--- 부서번호가 10 또는 30인 부서에서 급여가 2000 ~ 5000 사이의 사원의 사원명, 급여, 부서번호 출력
-select ENAME, SAL, DEPTNO from EMP e where (DEPTNO = 10 or DEPTNO = 30) and SAL between 2000 and 5000;
-select ENAME, SAL, DEPTNO from emp e where DEPTNO IN (10, 30) and SAL between 2000 and 5000;
-
--- 직업이 MANAGER 또는 SALESMAN 이고 급여가 1600, 2975, 2850이 아닌 사원명, 입사일, 급여, 부서번호 출력
-select ENAME, HIREDATE, SAL, DEPTNO from EMP e where JOB in ('MANAGER', 'SALESMAN') and SAL not in (1600, 2975, 2850);
-
-select * from dept d;
-
-/*=====================================================*/
-select * from EMP e where ENAME like 'S%';  -- 첫글자가 S인 문자열 출력
-select * from emp e where ename like '_L%'; -- 두번째 글자가 L인 문자열 출력
-
--- 사원 이름중 S가 포함되지 않는 부서번호 20인 사원의 사원명, 부서번호 출력
-select ENAME, DEPTNO from EMP e where ENAME not like '%S%' and DEPTNO = 20;
-
--- 1981년 6월 1일 ~ 12월 31일 입사자 중 부서명이 30인 사원의 부서번호, 사원명, 직업, 입사일 출력(입사일 오름차순 정렬)
-select DEPTNO, ENAME, JOB, HIREDATE from EMP e where HIREDATE between '1981-06-01' and '1981-12-31' and DEPTNO = 30;
-
-
+-- 2) 입사일이 1981.06.01 ~ 1981.12.31 입사자 중 부서명이 30인 사원의 부서번호, 사원명, 직업 입사일 조회(입사일 오름차순)
+SELECT DEPTNO, ENAME, JOB, HIREDATE
+FROM EMP
+WHERE HIREDATE BETWEEN TO_DATE('19810601', 'YYYYMMDD') AND TO_DATE('19811231', 'YYYYMMDD')
+  AND DEPTNO = 30
+ORDER BY HIREDATE ASC;
